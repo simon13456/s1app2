11643e78b43851851136b7c64b886780e1e43706
simon13456 <simonlerou@gmail.com>
2020-10-05 21:51:08 -0400

    Revert "fait"
    This reverts commit 91ce4e75413927efd76bedf87cd232257c44fd4f.

--- ValidationAPP2.c
+++ ValidationAPP2.c
@@ -2,15 +2,14 @@
 #define rangee 3
 #define colonne 3
 #define carre 3
-#define iteration 100
+#define iteration 10000
 void AdditionMat(int a[rangee][colonne],int b[rangee][colonne]);
 void MultiMat(int a[rangee][colonne],int b[rangee][colonne]);
 double sinus(float angle);
-float pwr(float base,int ala);
+int pwr(int base,int ala);
 long double factoriel(int nombre);
 int main(int argc, char **argv)
 {
-	printf("%f",sinus(0.5));
 	return 0;
 }
 void AdditionMat(int a[rangee][colonne],int b[rangee][colonne]){
@@ -63,7 +62,7 @@ double sinus(float angle){
 	return reponse;
 
 }
-float pwr(float base,int ala){
+int pwr(int base,int ala){
 	int reponse=base;
 	for(int i=1;i<ala; i++){
 			reponse*=base;
