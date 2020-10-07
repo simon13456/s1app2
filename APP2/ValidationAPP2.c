965bdef1d0a211e305377aa9ce8e73cc05b3d3a1
simon13456 <simonlerou@gmail.com>
2020-10-06 20:36:25 -0400

    fonction add et multi sont fix

--- APP2/ValidationAPP2.c
+++ APP2/ValidationAPP2.c
@@ -13,8 +13,8 @@ Description: Fonctions faisant des opérations mathématiques sans aucune librai
 #define termes 5
 #define pi 3.1415
 
-void AdditionMat(int a[rangee][colonne],int b[rangee][colonne]);
-void MultiMat(int a[carre][carre],int b[carre][carre]);
+void AdditionMat(int a[rangee][colonne],int b[rangee][colonne],int c[rangee][colonne]);
+void MultiMat(int a[carre][carre],int b[carre][carre],int c[carre][carre];);
 double sinus(float angle);
 float pwr(float base,int ala);
 long double factoriel(int nombre);
@@ -34,11 +34,19 @@ int main(int argc, char **argv)
 	printf("Le calcul du sinus avec l'angle en radian  %.4f est de : %.4f\n", (pi/4), sinus((pi/4)));
 	printf("Le calcul du sinus avec l'angle en radian  %.4f est de : %.4f\n", (pi/2), sinus((pi/2)));
 	
-	int a[rangee][colonne]={{2,2},{3,4},{5,6}};
+	int a[rangee][colonne]={{1,2},{3,4},{5,6}};
 	int b[rangee][colonne]={{6,5},{4,3},{2,1}};
-	
+	int c[rangee][colonne];
 	printf("\n L'addition des matrice a et b donne : \n" );
-	AdditionMat(a,b);
+	AdditionMat(a,b,c);
+	
+	for(int i=0;i<rangee;i++){
+		printf("|");
+		for(int j=0;j<colonne;j++){
+			printf(" %d",c[i][j]);
+		}
+		printf("|\n");
+	}
 	return 0;
 }
 /***************
@@ -99,19 +107,11 @@ int palindrome(char mot[])
  * Précondition:
  * Postcondition:
  * **************/
-void AdditionMat(int a[rangee][colonne],int b[rangee][colonne]){
-	int c [rangee][colonne];
+void AdditionMat(int a[rangee][colonne],int b[rangee][colonne],int c[rangee][colonne]){
 	for(int i=0;i<rangee;i++){
 		for(int j=0;j<colonne;j++){
 			c[i][j]=a[i][j]+b[i][j];
 		}
-	}
-	for(int i=0;i<rangee;i++){
-		printf("|");
-		for(int j=0;j<colonne;j++){
-			printf(" %d",c[i][j]);
-		}
-		printf("|\n");
 	}
 }
 /***************
@@ -119,24 +119,16 @@ void AdditionMat(int a[rangee][colonne],int b[rangee][colonne]){
  * Précondition:
  * Postcondition:
  * **************/
-void MultiMat(int a[carre][carre],int b[carre][carre]){
-	int temp=0;
-	int c[carre][carre];
+void MultiMat(int a[carre][carre],int b[carre][carre],int c[carre][carre]){
+	int temp=0;	
 	for (int i=0; i < 3; i++){
 		for (int j=0; j < 3; j++) {
 			for (int k=0; k < 3; k++) {
 				temp+=a[i][k]*b[k][j];
 			}
-			c[i][j]=temp;
+			c[i][j] = temp;
 			temp=0;
 		}
-	}
-	for(int i=0;i<carre;i++){
-		printf("|");
-		for(int j=0;j<carre;j++){
-			printf(" %d",c[i][j]);
-		}
-		printf("|\n");
 	}
 }
 /***************
