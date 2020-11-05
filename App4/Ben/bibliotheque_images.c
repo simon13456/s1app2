10d49f430272b2f3bb2e4b7d93a9db378a6ca1be
Benjamin <ben.gelinas@hotmail.com>
2020-11-05 10:41:46 -0500

    Fonctions PGM + PPM Ben (FINI)
    Normalement tout devrait être fini.

--- App4/Ben/bibliotheque_images.c
+++ App4/Ben/bibliotheque_images.c
@@ -12,10 +12,40 @@ int pgm_lire(char nom_fichier[], int matrice[MAX_HAUTEUR][MAX_LARGEUR], int *p_l
 	FILE *fichier;
 	char type[2]; 
 	fichier= fopen(nom_fichier,"r");
-	fscanf(fichier,"%s %s %s", p_metadonnees->auteur,p_metadonnees->dateCreation,p_metadonnees->lieuCreation);
+	if(fgetc(fichier)=='#'){
+		int p=0;
+		char c;
+		while((c=fgetc(fichier))!=';'){
+			p_metadonnees->auteur[p] = c;
+			p++;
+			
+		}
+		p=0;
+		fgetc(fichier);
+		while((c=fgetc(fichier))!=';'){
+			p_metadonnees->dateCreation[p] = c;
+			p++;
+		}
+		p=0;
+		fgetc(fichier);
+		while((c=fgetc(fichier))!='.'){
+			p_metadonnees->lieuCreation[p] = c;
+			p++;
+		}
+		p=0;
+	}
 	fscanf(fichier,"%s",type);
+	if(type[0]!='P'||type[1]!='2'){
+		return ERREUR_FORMAT;
+	}
 	fscanf(fichier,"%d %d", p_lignes,p_colonnes);
+	if((*p_lignes>MAX_HAUTEUR)||(*p_colonnes<MAX_LARGEUR)){
+		return ERREUR_TAILLE;
+	}
 	fscanf(fichier, "%d",p_maxval);
+	if(*p_maxval>MAX_VALEUR){
+		return ERREUR;
+	}
 	for(int i=0; i< *p_lignes ; i++){
 			for(int j=0; j<*p_colonnes; j++){
 				fscanf(fichier, "%d ",&matrice[i][j]);
@@ -134,7 +164,7 @@ int pgm_sont_identiques(int matrice1[MAX_HAUTEUR][MAX_LARGEUR], int lignes1, int
 		}
 	}
 }					
-	return OK;
+	return IDENTIQUES;
 }
 
 int pgm_pivoter90(int matrice[MAX_HAUTEUR][MAX_LARGEUR], int *p_lignes, int *p_colonnes, int sens){
@@ -165,4 +195,83 @@ int pgm_pivoter90(int matrice[MAX_HAUTEUR][MAX_LARGEUR], int *p_lignes, int *p_c
 	return OK;
 }
 
+int ppm_sont_identiques(struct RGB matrice1[MAX_HAUTEUR][MAX_LARGEUR], int lignes1, int colonnes1, struct RGB matrice2[MAX_HAUTEUR][MAX_LARGEUR], int lignes2, int colonnes2){
+	
+	if (lignes1 != lignes2 || colonnes1 != colonnes2){
+		return DIFFERENTES;
+	}
+	
+	for (int i=0; i<lignes1; i++){
+		for (int j=0; j<colonnes1; j++){
+			if (matrice1[i][j].valeurR != matrice2[i][j].valeurR)
+			{
+				return DIFFERENTES;
+			}
+			else if (matrice1[i][j].valeurG != matrice2[i][j].valeurG)
+			{
+				return DIFFERENTES;
+			}
+			else if (matrice1[i][j].valeurB != matrice2[i][j].valeurB)
+			{
+				return DIFFERENTES;
+			}
+		}
+	}
+	return IDENTIQUES;
+}
+
+int ppm_pivoter90(struct RGB matrice[MAX_HAUTEUR][MAX_LARGEUR], int *p_lignes, int *p_colonnes, int sens){
+		
+		
+	struct RGB temp[MAX_HAUTEUR][MAX_LARGEUR];	
+		
+	if (sens == SENS_HORAIRE){
+		for (int i=0; i<(*p_lignes/2); i++){
+			for (int j=i; j<(*p_colonnes-i-1); j++){
+				temp[i][j].valeurR = matrice[i][j].valeurR;	
+				matrice[i][j].valeurR = matrice[*p_colonnes-1-j][i].valeurR;
+				matrice[*p_colonnes-1-j][i].valeurR = matrice[*p_lignes-1-i][*p_colonnes-1-j].valeurR;
+				matrice[*p_lignes-1-i][*p_colonnes-1-j].valeurR = matrice[j][*p_lignes-1-i].valeurR; 
+				matrice[j][*p_lignes-1-i].valeurR = temp[i][j].valeurR;
+				
+				temp[i][j].valeurG = matrice[i][j].valeurG;	
+				matrice[i][j].valeurG = matrice[*p_colonnes-1-j][i].valeurG;
+				matrice[*p_colonnes-1-j][i].valeurG = matrice[*p_lignes-1-i][*p_colonnes-1-j].valeurG;
+				matrice[*p_lignes-1-i][*p_colonnes-1-j].valeurG = matrice[j][*p_lignes-1-i].valeurG; 
+				matrice[j][*p_lignes-1-i].valeurG = temp[i][j].valeurG;
+				
+				temp[i][j].valeurB = matrice[i][j].valeurB;	
+				matrice[i][j].valeurB = matrice[*p_colonnes-1-j][i].valeurB;
+				matrice[*p_colonnes-1-j][i].valeurB = matrice[*p_lignes-1-i][*p_colonnes-1-j].valeurB;
+				matrice[*p_lignes-1-i][*p_colonnes-1-j].valeurB = matrice[j][*p_lignes-1-i].valeurB; 
+				matrice[j][*p_lignes-1-i].valeurB = temp[i][j].valeurB; 	
+			}
+		}
+	}
+				
+	else if (sens == SENS_ANTIHORAIRE){
+		for (int i=0; i<(*p_lignes/2); i++){
+			for (int j=i; j<(*p_colonnes-i-1); j++){
+				temp[i][j].valeurR = matrice[i][j].valeurR;	
+				matrice[i][j].valeurR = matrice[j][*p_lignes-1-i].valeurR;
+				matrice[j][*p_lignes-1-i].valeurR = matrice[*p_lignes-1-i][*p_colonnes-1-j].valeurR;
+				matrice[*p_lignes-1-i][*p_colonnes-1-j].valeurR = matrice[*p_colonnes-1-j][i].valeurR; 
+				matrice[*p_colonnes-1-j][i].valeurR = temp[i][j].valeurR;
+				
+				temp[i][j].valeurG = matrice[i][j].valeurG;	
+				matrice[i][j].valeurG = matrice[j][*p_lignes-1-i].valeurR;
+				matrice[j][*p_lignes-1-i].valeurR = matrice[*p_lignes-1-i][*p_colonnes-1-j].valeurR;
+				matrice[*p_lignes-1-i][*p_colonnes-1-j].valeurR = matrice[*p_colonnes-1-j][i].valeurR; 
+				matrice[*p_colonnes-1-j][i].valeurR = temp[i][j].valeurR;
+				
+				temp[i][j].valeurR = matrice[i][j].valeurR;	
+				matrice[i][j].valeurR = matrice[j][*p_lignes-1-i].valeurR;
+				matrice[j][*p_lignes-1-i].valeurR = matrice[*p_lignes-1-i][*p_colonnes-1-j].valeurR;
+				matrice[*p_lignes-1-i][*p_colonnes-1-j].valeurR = matrice[*p_colonnes-1-j][i].valeurR; 
+				matrice[*p_colonnes-1-j][i].valeurR = temp[i][j].valeurR;
+			}
+		}
+	}	
+	return OK;
+}
 
